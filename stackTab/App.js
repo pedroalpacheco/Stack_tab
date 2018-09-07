b77a383e2fa5cb3e0d951066b436a5ad328c57dc
pedro.pacheco.a@gmail.com <pedro.pacheco.a@gmail.com>
2018-09-07 15:02:07 -0300

    Inicio

---
+++ stackTab/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { TabNavigator } from 'react-navigation';
+
+import TelaInicial from './src/TelaInicial';
+import ConversationScreen from './src/ConversationScreen';
+import ConfigScreen from './src/ConfigScreen';
+
+const Navegador = TabNavigator({
+	Home: {
+		screen:TelaInicial
+	},
+	Conversa: {
+		screen:ConversationScreen
+	},
+  Config: {
+    screen:ConfigScreen
+  }
+});
+export default Navegador;
